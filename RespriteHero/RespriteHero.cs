c76c5751ed2cfa3c275f820ba4835e3156e9efee
Shiru <Shirubaurufu@users.noreply.github.com>
2022-06-12 01:00:47 +0100

    Allowed high & low res sprites

--- RespriteHero/RespriteHero.cs
+++ RespriteHero/RespriteHero.cs
@@ -24,20 +24,22 @@ namespace RespriteHero {
 			}
 		}
 
-		public static Sprite LoadPNGCache(string filePath) {
+		public static Sprite LoadPNGCache(string filePath, int[] gridNum) {
 			if (SPRITE_CACHE.ContainsKey(filePath)) {
 				return SPRITE_CACHE[filePath];
 			}
 			else {
-				Sprite sprite = LoadPNG(filePath);
+				Sprite sprite = LoadPNG(filePath, gridNum);
 				SPRITE_CACHE.Add(filePath, sprite);
 				return sprite;
 			}
 		}
 
-		public static Sprite LoadPNG(string filePath) {
+		public static Sprite LoadPNG(string filePath, int[] gridNum) {
 			Texture2D tex = null;
 			byte[] fileData;
+
+			float PPU = 16;
 
 			if (File.Exists(filePath)) {
 				fileData = File.ReadAllBytes(filePath);
@@ -48,13 +50,26 @@ namespace RespriteHero {
 				MelonLogger.Msg("NOT FOUND: " + filePath);
 			}
 
+			if (tex.width > tex.height)
+				PPU = tex.width / gridNum[0];
+			else
+				PPU = tex.height / gridNum[1];
+
+			if (PPU % 16 >= 8)
+				PPU += 16 - (PPU % 16);
+			else if ((PPU % 16) != 0)
+				PPU -= PPU % 16;
+
 			tex.filterMode = FilterMode.Point;
+			
 
 			return Sprite.Create(
 				tex,
 				new Rect(0, 0, tex.width, tex.height),
 				new Vector2(0.5f, 0.5f),
-				16f //Same value as Backpack Hero PPU
+				16f, //Same value as Backpack Hero PPU
+				0, 
+				SpriteMeshType.FullRect
 			);
 		}
 	}
@@ -87,7 +102,23 @@ namespace RespriteHero {
 					MelonLogger.Msg($"[ItemSpriteChanger]: Opening filename[{filename}]");
 
 					if (RespriteHero.SPRITE_CACHE.ContainsKey(filename) || File.Exists(filename)) {
-						Sprite newSprite = RespriteHero.LoadPNGCache(filename);
+						BoxCollider2D[] boxColliders = __instance.gameObject.GetComponentsInChildren<BoxCollider2D>();
+						int[] gridNum = new int[2] { 1, 1 };
+						Vector2 max = new Vector2 { x = 0, y = 0 };
+						Vector2 min = new Vector2 { x = 0, y = 0 };
+						if (boxColliders.Length > 0) {
+							foreach (BoxCollider2D collider in boxColliders) {
+								max.x = collider.bounds.max.x + collider.offset.x - __instance.transform.parent.position.x;
+								max.y = collider.bounds.max.y + collider.offset.y - __instance.transform.parent.position.y;
+								min.x = collider.bounds.min.x + collider.offset.x - __instance.transform.parent.position.x;
+								min.y = collider.bounds.min.y + collider.offset.y - __instance.transform.parent.position.y;
+							}
+						}
+
+						gridNum[0] = Mathf.CeilToInt(Mathf.Abs(max.x) + Mathf.Abs(min.x));
+						gridNum[1] = Mathf.CeilToInt(Mathf.Abs(max.y) + Mathf.Abs(min.y));
+
+						Sprite newSprite = RespriteHero.LoadPNGCache(filename, gridNum);
 						sprites[i] = newSprite;
 					}
 					else {
@@ -102,7 +133,22 @@ namespace RespriteHero {
 
 			if (RespriteHero.SPRITE_CACHE.ContainsKey(filename) || File.Exists(filename)) {
 				MelonLogger.Msg($"[SpriteRenderer]: Opening filename[{filename}]");
-				Sprite newSprite = RespriteHero.LoadPNGCache(filename);
+				BoxCollider2D[] boxColliders = __instance.gameObject.GetComponentsInChildren<BoxCollider2D>();
+				int[] gridNum = new int[2] { 1, 1 };
+				Vector2 max = new Vector2 { x = 0, y = 0 };
+				Vector2 min = new Vector2 { x = 0, y = 0 };
+				if (boxColliders.Length > 0) {
+					foreach (BoxCollider2D collider in boxColliders) {
+						max.x = collider.bounds.max.x + collider.offset.x - __instance.transform.parent.position.x;
+						max.y = collider.bounds.max.y + collider.offset.y - __instance.transform.parent.position.y;
+						min.x = collider.bounds.min.x + collider.offset.x - __instance.transform.parent.position.x;
+						min.y = collider.bounds.min.y + collider.offset.y - __instance.transform.parent.position.y;
+					}
+				}
+
+				gridNum[0] = Mathf.CeilToInt(Mathf.Abs(max.x) + Mathf.Abs(min.x));
+				gridNum[1] = Mathf.CeilToInt(Mathf.Abs(max.y) + Mathf.Abs(min.y));
+				Sprite newSprite = RespriteHero.LoadPNGCache(filename, gridNum);
 				SpriteRenderer spriteRenderer = __instance.gameObject.GetComponent<SpriteRenderer>();
 				spriteRenderer.sprite = newSprite;
 			}
